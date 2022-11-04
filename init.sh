b02feb6d1b07f878df353f8ee0eaebcdc2ef96eb
protowood <v_2rwagg@hotmail.com>
2022-11-04 12:22:55 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-useradd -m -s /bin/bash anisble
+useradd -m -s /bin/bash ansible
 mkdir /home/ansible/.ssh
 chmod 700 /home/ansible/.ssh
 wget https://raw.githubusercontent.com/protowood/stuff/main/id_rsa.pub /home/ansible/.ssh/authorized_keys
-chmod 600 /home/ansible/.ssh/authorized_keys
-restorecon -R -v /root/.ssh
+chmod 600 /home/ansible/.ssh/id_rsa.pub
+chown -R ansible:ansible /home/ansible/.ssh
+restorecon -R -v /home/ansible/.ssh
